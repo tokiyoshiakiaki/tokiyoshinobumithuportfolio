9ff6d518a934e5a3c69422e8e0d5011d1098fd1c
tokiyoshiakiaki <109967641+tokiyoshiakiaki@users.noreply.github.com>
2022-10-10 01:16:00 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+$(function (){
+    $('.list-btn, .logo, .navi-list-item > a').on('click',function () {
+        $('.list-btn, .header, .logo-responsive').toggleClass('active');
+    });
+});
+
+$(function () {
+    $('.navi-list a[href*="#"]').click(function () {
+        var elmHash = $(this).attr('href');
+        var pos = $(elmHash).offset().top;
+        $('body,html').animate({scrollTop: pos}, 900);
+        return false;
+    });
+});
+
+$(function () {
+   var block = $('.fadein'),
+       Window = $(window);
+
+        Window.scroll(function () {
+        block.each(function () {
+            if(Window.scrollTop() > $(this).offset().top - Window.height()) {
+                $(this).addClass("show");
+            }
+        });
+    });  
+});
+ 
